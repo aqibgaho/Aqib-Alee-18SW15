fc3b2fc71e622131099c1d90651abca989a448bb
aqibgaho <49454864+aqibgaho@users.noreply.github.com>
2019-04-17 13:11:49 +0500

    Add files via upload

---
+++ Java/Lab No 1/TaskOne.java
@@ -0,0 +1,27 @@
+class TaskOne{
+	public static void main(String args[]){
+		String n;
+		byte a;
+		short m;
+		float per;
+		String stat;
+		char g;
+                double p;
+                int y;
+
+		n = "Aaqib";
+		a = 18;
+		m = 500;
+		per = 87.67f;
+        stat = "Pass";
+        g = 'A';
+        p = 88.5;
+        y = 2018;
+        System.out.println(" Name: "+n+"\n Age: "+a+"\n Marks: "+m+"\n Percentage: "+per+"\n Status: "+stat+"\n Grade: "+g+"\n Class Attendance Percentage: "+p+"\n Year: "+y);
+        
+
+
+
+
+	}//end of main
+}//end of class
---
+++ Java/Lab No 1/TaskTwo.java
@@ -0,0 +1,11 @@
+class TaskTwo{
+	public static void main(String args[]){
+	double d = 50.567;
+	long l = (int)d;
+    double e = d - l;
+    System.out.println("Number: " + d + "\nWhole Nummber: " + l + " \nPoint Number : " + e ); 
+}//end of main
+
+
+
+}//end of class
